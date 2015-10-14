c7562058b806503f41d22e360b5ba4feb7fcb512
Jin Wei <453251481@qq.com>
2015-10-14 14:51:00 +0100

    Skeleton User profile page support

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   
+  resources :users 
+  root 'static_pages#home'
 
-    root 'static_pages#home'
-
-    get 'help'    => 'static_pages#help'
-    get 'about'   => 'static_pages#about'
-    get 'contact' => 'static_pages#contact'
-    get 'signup'  => 'users#new'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
 
 end
 
