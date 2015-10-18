e7a032b3d0720f14f86347607fbd784658316ed0
Jin Wei <453251481@qq.com>
2015-10-18 22:15:05 +0100

    Finish user signup

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,5 +6,6 @@ class StaticPagesController < ApplicationController
   end
 
   def about
+  	#flash[:notice] = "Testing the flash"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,18 @@ class UsersController < ApplicationController
 	end
 
 	def new
-	end
+        @user = User.new
+    end
+
+    def create
+    secure_params = params.require(:user).permit(:name, :email, 
+                              :password, :password_confirmation)
+    @user = User.new(secure_params)
+    if @user.save
+        flash[:success] = "Welcome to the Twitter App!"
+        redirect_to @user# Handle a successful save.
+    else
+        render 'new' # Handle an unsuccessful save.     
+    end
+    end
 end
