f2c3a4253dff523c2ddfa26d362c5303439f3b8c
Jin Wei <453251481@qq.com>
2015-11-04 21:30:16 +0000

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -36,5 +36,10 @@ module SessionsHelper
       forget(current_user)
       @current_user = nil
   end
-
+  def logged_in_user
+      unless logged_in?
+        flash[:notice] = "Please log in"
+        redirect_to login_url
+      end
+  end 
 end
